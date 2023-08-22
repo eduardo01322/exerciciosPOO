dda6f928c661fbb5e5266c27d398a6a17b6d08fd
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-22 10:44:28 -0300

    novo

---
+++ cachorro.ts
@@ -0,0 +1,25 @@
+class Cachorro{
+    nome: string;
+    raca: string;
+    idade: number;
+
+    constructor(nome: string, raca: string, idade: number){
+        this.nome = nome;
+        this.raca = raca;
+        this.idade = idade;
+    }
+    apresentar(){
+        return " Oi, sou " + this.nome + " um " + this.raca + " de " + this.idade + " anos "
+    }
+    apresentarPara(humano: string){
+        return "Ola " + humano + " eu sou " + this.nome + " um " + this.raca + " de " + this.idade + " anos "
+    }
+}
+
+let rex = new Cachorro("Rex", "Labrador", 3);
+let torss = new Cachorro("torss", "vira-lata", 16 );
+
+console.log(rex.nome);
+console.log(torss.nome);
+console.log(rex.apresentarPara("regis"));
+console.log(torss.apresentarPara("regis"));
