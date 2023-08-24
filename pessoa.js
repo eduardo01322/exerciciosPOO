e2534ac414f4729c8913a74cc90a82c6cb794b3d
eduardo01322 <eduardo.errea32@gmail.com>
2023-08-24 10:38:22 -0300

    ex.

---
+++ pessoa.js
@@ -0,0 +1,24 @@
+"use strict";
+class Pessoa {
+    constructor(nome, idade, comidas) {
+        this.nome = nome;
+        this.idade = idade;
+        this.comidas = comidas;
+    }
+    cumprimentar() {
+        let msgHabilitacao = " já posso tirar minha CNH";
+        if (this.idade < 18) {
+            msgHabilitacao = " não posso tirar minha CNH";
+        }
+        return "Olá meu nome é " + this.nome + " e tenho " + this.idade + " anos" + msgHabilitacao + ".";
+    }
+    comidasFavoritas() {
+        console.log("Minha(s) comida(s) favorita(s)");
+        for (let i = 0; i <= this.comidas.length; i++) {
+            console.log(this.comidas[i]);
+        }
+    }
+}
+let pessoal = new Pessoa("joão", 17, ["doguinho", "macarrão"]);
+console.log(pessoal.cumprimentar());
+console.log(pessoal.comidasFavoritas());
---
+++ produtos.js
@@ -0,0 +1,22 @@
+"use strict";
+class produtos {
+    constructor(produto, preço) {
+        this.produto = produto;
+        this.preço = preço;
+    }
+    Produtos() {
+        return "Produto: " + this.produto + "Preço: R$ " + this.preço;
+    }
+    descontos(desconto) {
+        return "Desconto de " + desconto + "%. Novo preço R$" + (this.preço - this.preço * (desconto / 100));
+    }
+}
+let produto1 = new produtos("nintendo", 2000);
+let produto2 = new produtos("playstation", 1500);
+let produto3 = new produtos("xbox", 2500);
+console.log(produto1.Produtos());
+console.log(produto1.descontos(0));
+console.log(produto2.Produtos());
+console.log(produto1.descontos(50));
+console.log(produto3.Produtos());
+console.log(produto1.descontos(100));
