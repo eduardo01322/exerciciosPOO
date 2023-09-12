fff0e13a1683133d14b741b79ca617e934ecc3b2
eduardo01322 <eduardo.errea32@gmail.com>
2023-09-12 10:53:47 -0300

    loja

--- loja.ts
+++ loja.ts
@@ -1,4 +1,4 @@
-class produto{
+class produtoLoja{
     nome: string;
     preço: number;
 
@@ -8,21 +8,71 @@ class produto{
     }
 }
 class carrinho{
-    produtos: produto[] = [];
+    produtos: produtoLoja[] = [];
 
     constructor(){}
 
-adicionarProduto(produtos: produto) {
+adicionarProduto(produtos: produtoLoja){
     this.produtos.push(produtos);
 }
-listarEventos() {
+listarCarrinho() {
     console.log("Meu carrinho");
     for(let i=0; i<this.produtos.length; i++){
-    console.log(this.produtos[i].nome+"R$"+this.produtos[i].preço);
+    console.log(this.produtos[i].nome+" R$ "+this.produtos[i].preço);
 }
 }
-apagar(produtos:produto) {
-    this.produtos= this.produtos.filter(item =>item.nome!= produtos.nome);
+apagar(produto:produtoLoja){
+    this.produtos= this.produtos.filter(item =>item.nome!= produto.nome);
     return "Produto apagado"
 }
+preço(){
+    let soma =0;
+    for (let i=0; i<this.produtos.length; i++){
+        soma=soma+this.produtos[i].preço;
+    }console.log ("Sua compra é de "+(soma.toFixed(2)))
 }
+}
+class loja{
+    estoque: produtoLoja[] = [];
+    carrinho= new carrinho();
+
+    constructor(){}
+    
+    adicionarEstoque(estoque: produtoLoja){
+        this.estoque.push (estoque);
+    }
+    listarEstoque(){
+        console.log("estoque");
+        for(let i=0; i<this.estoque.length; i++){
+        console.log(this.estoque[i].nome+" R$ "+this.estoque[i].preço);
+    }
+    }
+    apagarEstoque(estoque:produtoLoja) {
+        this.estoque=this.estoque.filter(item =>item.nome!= estoque.nome);
+        return "Produto do estoque apagado"
+    }
+}
+
+const objeto1 = new produtoLoja ("picanha", 13.00)
+const objeto2 = new produtoLoja ("bolsa", 22.00)
+const objeto3 = new produtoLoja ("torta", 17.00)
+const objeto4 = new produtoLoja ("calça", 50.00)
+const meucarrinho = new carrinho();
+meucarrinho.adicionarProduto(objeto1);
+meucarrinho.adicionarProduto(objeto2);
+meucarrinho.adicionarProduto(objeto3);
+meucarrinho.adicionarProduto(objeto4);
+console.log(meucarrinho.listarCarrinho());
+console.log(meucarrinho.preço());
+console.log(meucarrinho.apagar(objeto2));
+console.log(meucarrinho.listarCarrinho());
+console.log(meucarrinho.preço());console.log("");
+const estoque = new loja();
+estoque.adicionarEstoque(objeto1);
+estoque.adicionarEstoque(objeto2);
+estoque.adicionarEstoque(objeto3);
+estoque.adicionarEstoque(objeto4);
+console.log(estoque.listarEstoque());
+console.log(estoque.apagarEstoque(objeto2));
+console.log(estoque.listarEstoque());
+
