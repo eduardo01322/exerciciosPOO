2f25d6edcc18d1f788fc2e11baca9ef5bcdd7b39
eduardo01322 <eduardo.errea32@gmail.com>
2023-09-26 14:52:10 -0300

    ex2

--- ContaBancaria.js
+++ ContaBancaria.js
@@ -13,12 +13,14 @@ class ContaCorrente extends ContaBancaria2 {
     }
     Depositar(valor) {
         this.saldo = this.saldo + valor;
-        console.log("Você depositou R$ " + valor + "seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo);
+        console.log("Você depositou R$ " + valor + " seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo);
     }
     sacar(valor) {
         this.saldo = this.saldo - valor;
         let pegar = " você sacou R$ " + valor + " com sucesso, seu saldo atual é de R$ " + this.saldo;
-        if (valor > this.limiteChequeEspecial) {
+        if (valor > this.limiteChequeEspecial)
+            (valor > this.saldo);
+        {
             pegar = " Você não tem saldo o sufuciente. Seu saldo atual é de R$ " + (this.saldo + valor);
         }
         console.log(this.titular + pegar);
@@ -34,7 +36,7 @@ class ContaPoupanca extends ContaBancaria2 {
     }
     Depositar(valor) {
         this.saldo = this.saldo + valor;
-        console.log("Você depositou R$ " + valor + "seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo);
+        console.log("Você depositou R$ " + valor + " seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo);
     }
     sacar(valor) {
         this.saldo = this.saldo - valor;
@@ -45,12 +47,51 @@ class ContaPoupanca extends ContaBancaria2 {
         console.log(this.titular + pegar);
     }
     consultarSaldo() {
+        console.log("seu aldo atual é de " + this.saldo);
         return this.saldo;
     }
     calcularRendimentoAnual(taxa) {
+        this.saldo = taxa * this.saldo;
+        console.log("Seu rendimento Anul foi de R$" + this.saldo);
+        return this.saldo;
+    }
+}
+class ContaInvestimento extends ContaBancaria2 {
+    constructor() {
+        super("qwer", 100, "qwert");
+    }
+    Depositar(valor) {
+        this.saldo = this.saldo + valor;
+        console.log("Você depositou R$ " + valor + " seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo);
+    }
+    sacar(valor) {
+        this.saldo = this.saldo - valor;
+        let pegar = " você sacou R$ " + valor + " com sucesso, seu saldo atual é de R$ " + this.saldo;
+        if (valor > this.saldo) {
+            pegar = " Você não tem saldo o sufuciente. Seu saldo atual é de R$ " + (this.saldo + valor);
+        }
+        console.log(this.titular + pegar);
+    }
+    consultarSaldo() {
+        console.log("seu aldo atual é de " + this.saldo);
+        return this.saldo;
+    }
+    investir(valor, prazoMeses, Juros) {
+        this.saldo = valor * (1 + Juros) * prazoMeses;
+        console.log("Olá" + this.titular + " da conta de numero" + this.numeroConta + " seu investimento de " + valor + " deu resultados, agora você tem R$" + this.saldo);
     }
 }
 const qqq = new ContaCorrente();
 qqq.Depositar(100);
 qqq.sacar(2000);
 qqq.consultarSaldo();
+const rrr = new ContaPoupanca();
+rrr.Depositar(100);
+rrr.sacar(2000);
+rrr.consultarSaldo();
+rrr.calcularRendimentoAnual(0.2);
+const uuu = new ContaInvestimento();
+uuu.Depositar(100);
+uuu.sacar(2000);
+uuu.consultarSaldo();
+uuu.investir(100, 12, 0.2);
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -23,13 +23,13 @@ class ContaCorrente extends ContaBancaria2{
 
     Depositar(valor: number): void{
         this.saldo = this.saldo + valor
-        console.log("Você depositou R$ " + valor + "seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo) 
+        console.log("Você depositou R$ " + valor + " seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo) 
     }
 
     sacar(valor: number): void {
         this.saldo = this.saldo - valor
         let pegar = " você sacou R$ " + valor + " com sucesso, seu saldo atual é de R$ " + this.saldo;
-        if(valor > this.limiteChequeEspecial) 
+        if(valor > this.limiteChequeEspecial) (valor > this.saldo)
         {
          pegar = " Você não tem saldo o sufuciente. Seu saldo atual é de R$ " + (this.saldo + valor);
         }
@@ -49,7 +49,7 @@ class ContaPoupanca extends ContaBancaria2{
 
     Depositar(valor: number): void{
         this.saldo = this.saldo + valor
-        console.log("Você depositou R$ " + valor + "seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo) 
+        console.log("Você depositou R$ " + valor + " seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo) 
     }
 
     sacar(valor: number): void {
@@ -63,15 +63,63 @@ class ContaPoupanca extends ContaBancaria2{
     }
     
     consultarSaldo(): number{
+        console.log("seu aldo atual é de " + this.saldo);
         return this.saldo
     }
 
     calcularRendimentoAnual(taxa: number): number{
-        
+        this.saldo=taxa * this.saldo;
+        console.log("Seu rendimento Anul foi de R$"+this.saldo)
+        return this.saldo
     }
+}
+
+class ContaInvestimento extends ContaBancaria2{
+    constructor(){
+        super("qwer", 100, "qwert")
+    }
+
+    Depositar(valor: number): void{
+        this.saldo = this.saldo + valor
+        console.log("Você depositou R$ " + valor + " seu dinheiro foi depositado com suscesso. Seu saldo atual é de R$" + this.saldo) 
+    }
+
+    sacar(valor: number): void {
+        this.saldo = this.saldo - valor
+        let pegar = " você sacou R$ " + valor + " com sucesso, seu saldo atual é de R$ " + this.saldo;
+        if(valor > this.saldo) 
+        {
+         pegar = " Você não tem saldo o sufuciente. Seu saldo atual é de R$ " + (this.saldo + valor);
+        }
+        console.log(this.titular + pegar)
+    }
+    
+    consultarSaldo(): number{
+        console.log("seu aldo atual é de " + this.saldo);
+        return this.saldo
+    }
+
+    investir(valor: number, prazoMeses: number, Juros: number): void{
+           this.saldo = valor*(1+Juros)*prazoMeses
+        console.log("Olá" + this.titular + " da conta de numero" + this.numeroConta + " seu investimento de " + valor + " deu resultados, agora você tem R$" + this.saldo);
+
+
+        }
 }
 
 const qqq = new ContaCorrente();
 qqq.Depositar(100);
 qqq.sacar(2000);
 qqq.consultarSaldo();
+
+const rrr = new ContaPoupanca();
+rrr.Depositar(100);
+rrr.sacar(2000);
+rrr.consultarSaldo();
+rrr.calcularRendimentoAnual(0.2);
+
+const uuu = new ContaInvestimento();
+uuu.Depositar(100);
+uuu.sacar(2000);
+uuu.consultarSaldo();
+uuu.investir(100, 12, 0.2);
