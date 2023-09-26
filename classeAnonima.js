68bf1cfb24140fdc9f454a7a70d190ee5a6452e4
eduardo01322 <eduardo.errea32@gmail.com>
2023-09-26 09:19:27 -0300

    anonima

--- classeAnonima.js
+++ classeAnonima.js
@@ -9,3 +9,21 @@ const pessoa = new class {
     }
 };
 console.log(pessoa.getNomeCompleto());
+const treinadorPessoal = new class {
+    constructor() {
+        this.rotina = [];
+    }
+    adicionarExercicios(nome, duracao) {
+        this.rotina.push({ nome, duracao });
+    }
+    calcularTempoTotal() {
+        let soma = 0;
+        this.rotina.forEach(item => soma += item.duracao);
+        // this.rotina.forEach(item => soma + soma + item.duracao);
+        return soma;
+    }
+};
+treinadorPessoal.adicionarExercicios("Corrida", 30);
+treinadorPessoal.adicionarExercicios("flexoes", 15);
+treinadorPessoal.adicionarExercicios("agachamento", 20);
+console.log("tempo total necessário para a rotina: " + treinadorPessoal.calcularTempoTotal() + " minutos");
--- classeAnonima.ts
+++ classeAnonima.ts
@@ -1,10 +1,29 @@
 const pessoa = new class {
     private primeiroNome = "José";
     private ultimoNome = "da Silva";
-    
+
     getNomeCompleto(){
         return this.primeiroNome + " " + this.ultimoNome;
     }
 }
 
 console.log(pessoa.getNomeCompleto());
+
+const treinadorPessoal = new class{
+    private rotina: {nome: string; duracao: number}[] = [];
+    adicionarExercicios(nome: string, duracao: number): void {
+        this.rotina.push({nome, duracao});
+    }
+
+    calcularTempoTotal():number{
+        let soma = 0;
+        this.rotina.forEach(item => soma += item.duracao);
+       // this.rotina.forEach(item => soma + soma + item.duracao);
+        return soma;
+    }
+}
+
+treinadorPessoal.adicionarExercicios("Corrida", 30);
+treinadorPessoal.adicionarExercicios("flexoes", 15);
+treinadorPessoal.adicionarExercicios("agachamento", 20);
+console.log("tempo total necessário para a rotina: " + treinadorPessoal.calcularTempoTotal()+" minutos");
